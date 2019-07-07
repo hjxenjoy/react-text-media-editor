f48ccc48088c5577c58c7774633c26facba65aa6
Jinxin Hu <hjxenjoy@gmail.com>
2019-07-07 16:05:09 +0800

    chore(port): update webpack dev server port

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -48,7 +48,7 @@ module.exports = {
     // 0.0.0.0 is available to all network devices
     // unlike default `localhost`.
     // host: process.env.HOST, // Defaults to `localhost`
-    port: 2019, // Defaults to 8080
+    port: 19707, // Defaults to 8080
     open: true, // Open the page in browser
     hot: true,
     overlay: true,
