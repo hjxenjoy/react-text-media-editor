f2a9c7801986416837c51be20d90d90bb2bfc22a
hjxenjoy <hjxenjoy@users.noreply.github.com>
2019-09-12 06:13:00 +0000

    Deploying to gh-pages - 06:13:00

---
+++ precache-manifest.20c2e9cd54cdaa79fcffcff52532c264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6be7f952aa4fdf014f1b7c49fa72ed",
+    "url": "https://hujin.xin/react-text-media-editor/index.html"
+  },
+  {
+    "revision": "225722f89ed75324498d",
+    "url": "https://hujin.xin/react-text-media-editor/static/css/main.903c5942.chunk.css"
+  },
+  {
+    "revision": "bd03ffefbe50e7f6bd01",
+    "url": "https://hujin.xin/react-text-media-editor/static/js/2.ff500214.chunk.js"
+  },
+  {
+    "revision": "225722f89ed75324498d",
+    "url": "https://hujin.xin/react-text-media-editor/static/js/main.3f567162.chunk.js"
+  },
+  {
+    "revision": "bd3c955130a0d68398d1",
+    "url": "https://hujin.xin/react-text-media-editor/static/js/runtime~main.6be93b58.js"
+  }
+]);
